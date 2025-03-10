b49790df25784964133d6f2b56ea2049105cdc43
willnight01 <willnightzhanglixia@126.com>
2025-03-10 23:19:56 +0800

    tag 1.1.0

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World from Spring Boot!this is test feature，this is git tag v0.0.1";
+        return "Hello World from Spring Boot!this is test feature，this is git tag v1.0";
     }
 } 
